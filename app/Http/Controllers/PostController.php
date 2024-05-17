5aa6e7d3dd9d4f2cf43a3d441f0c6ab1ead6d9d5
Yber-M <ybermarlon@gmail.com>
2024-05-17 12:12:17 -0500

    Creando contrladores a través de consola

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,12 +1,15 @@
 <?php
 
+//TODO: COMANDO USADO EN TERMINAL -> php artisan make:controller PostController
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $posts = [
             ['title' => 'First Post'],
             ['title' => 'Second Post'],
@@ -14,6 +17,6 @@ class PostController extends Controller
             ['title' => 'Fourth Post']
         ];
 
-        return view('blog', ['posts'=> $posts]);
+        return view('blog', ['posts' => $posts]);
     }
 }
