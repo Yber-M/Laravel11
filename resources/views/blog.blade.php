70c628ce986dafc416d7aece8f3886d6c9122c47
Yber-M <ybermarlon@gmail.com>
2024-05-17 12:20:14 -0500

    Se comento el dump

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,11 +1,11 @@
 <x-layouts.app title="BLOG" meta-description="BLOG Meta Description">
 
-    <h1>Este es mi primer BLOG</h1>
+    <h1 style="color: rgb(175, 59, 59)">Este es mi primer BLOG</h1>
 
-    @dump($posts)
-
+    {{-- @dump($posts) --}}
+    <h2 style="color: green">ARREGLOS:</h2>
     @foreach ($posts as $post)
-        <h2>{{ $post['title'] }}</h2>
+        <h3 style="color: yellow">{{ $post['title'] }}</h3>
     @endforeach
-    
+
 </x-layouts.app>
