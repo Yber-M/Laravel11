f4a120499efd3e82961c2f3e36c3f7d474c57d0a
Yber-M <ybermarlon@gmail.com>
2024-05-23 14:48:16 -0500

    Test

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-
+//* Definimos nesutra ruta
 Route::view('/', 'welcome')->name('home');
 
 Route::view('/contacto', 'contacto')->name('contact');
@@ -11,3 +11,4 @@ Route::view('/contacto', 'contacto')->name('contact');
 Route::get('/blog', [PostController::class, 'index'])->name('blog');
 
 Route::view('/about', 'about')->name('about');
+
