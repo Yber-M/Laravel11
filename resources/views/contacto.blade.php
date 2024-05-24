79e998979b634a5c16d401a273135e4bb7d55d82
Yber-M <ybermarlon@gmail.com>
2024-05-24 01:18:42 -0500

    Se agregaron mas comentarios

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,9 +1,10 @@
-{{-- TODO -> <x-layouts.section/> es equivalente a @component('components.layouts.section') --}}
+{{--OjO -> <x-layouts.section/> es equivalente a @component('components.layouts.section') --}}
 
 @component('components.layouts.section')
 
     {{--? Lo bueno del componente de blade es qye se puede agregar otras propiedades dentro --}}
     <x-layouts.section 
+    {{-- OjO -> No se puede usar las propiedades del componente --}}
         title="Contácto" 
         meta-description="Contacto Meta Description"> {{-- ? Se usa snake-case para llamar al atributo--}}
             
