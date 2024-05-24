acc0b38137e72e995a86d82c086041e4f658e79f
Yber-M <ybermarlon@gmail.com>
2024-05-24 02:22:18 -0500

    foreach para recorrer el array

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,9 +6,11 @@
     <h1>ABOUT CHI k no?</h1>
     <hr>
     <h1>ARRAY PRE ESTABLECIDA</h1>
+
     @foreach ($dtPipol as $almacenador)
         <h2 style="color: rgb(107, 155, 35)">
             {{ $almacenador['numPerson'] }}
         </h2>
     @endforeach
+    
 </x-layouts.section>
