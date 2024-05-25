9419c40772c9e2472650d49ef1323c5dae946621
Yber-M <ybermarlon@gmail.com>
2024-05-25 01:41:08 -0500

    Se agrega el al controlador POST la conexion con la DB para obtener los datos de la tabla POSTS

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,7 +6,7 @@
     <h1 style="color: rgb(175, 59, 59)">Este es mi primer BLOG</h1>
 
     {{--// @dump($posts) --}}
-    <h2 style="color: green">ARREGLOS:</h2>
+    <h2 style="color: green">ARREGLOS USANDO LA DB:</h2>
     
     @foreach ($posts as $post)
         <h3 style="color: yellow">{{ $post -> title }}</h3>
