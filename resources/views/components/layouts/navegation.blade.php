ae0136a263c989e0f42b72672a2038210d9aa7b8
Yber-M <ybermarlon@gmail.com>
2024-05-26 18:34:52 -0500

    Actualizamos la nueva ruta de blog

--- resources/views/components/layouts/navegation.blade.php
+++ resources/views/components/layouts/navegation.blade.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="{{ route('inicio') }}">Inicio</a></li>
-    <li><a href="{{ route('blog') }}">Blog</a></li>
+    <li><a href="{{ route('posts.index') }}">Blog</a></li>
     <li><a href="{{ route('about') }}">About</a></li>
     <li><a href="{{ route('contacto') }}">Contácto</a></li>
     <li><a href="{{ route('usando') }}">Usando Controlador</a></li>
