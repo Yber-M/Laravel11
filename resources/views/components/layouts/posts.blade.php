3a3dec7639ec30653e4371e82afac346e2526372
Yber-M <ybermarlon@gmail.com>
2024-05-26 20:15:20 -0500

    Se cambio el titul

--- resources/views/components/layouts/posts.blade.php
+++ resources/views/components/layouts/posts.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Datos | {{ $title ?? 'User' }}</title>
+    <title>Post | {{ $title ?? 'Action' }}</title>
     <meta name="description" content="{{ $metaDescription ?? 'Valor Por Defecto de Descripcción' }}">
 </head>
 
