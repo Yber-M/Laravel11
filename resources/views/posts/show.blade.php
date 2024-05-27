21b61da5ddd5d4100440f56bbee32429297b77d6
Yber-M <ybermarlon@gmail.com>
2024-05-26 20:14:13 -0500

    Se muestra los datos de las nuevas columnas

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,9 +3,12 @@
     :meta-description="$post->dni"
 >
 
-    <h1>Datos del Post N° {{ $post->id }}</h1>
+    <h1>Post N° {{ $post->id }}</h1>
+    <p>Autor: {{ $post->nombre }} </p>
     <p>DNI: {{ $post->dni }}</p>
+    <p>Edad: {{ $post->edad}} </p>
     <p>Titulo: {{ $post->title }}</p>
+    <p> {{ $post->cuerpo }} </p>
     <p>Fecha de creación: {{ $post->created_at }}</p>
     
     <a href=" {{ route('posts.index') }}">
