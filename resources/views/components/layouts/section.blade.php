119bbfb02923a54562f773f56f2943f2737f1de3
Yber-M <ybermarlon@gmail.com>
2024-05-28 17:59:56 -0500

    Implementamos Bootstrap

--- resources/views/components/layouts/section.blade.php
+++ resources/views/components/layouts/section.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>GLOBTECx | {{ $title ?? 'Sin titulo' }}</title>
-    
+
     {{-- OjO -> Se usa camelCase para referenciar la propiedad --}}
     <meta name="description" content="{{ $metaDescription ?? 'No tiene metadescription' }}">
 </head>
